189c2dd72fdd150bda288c2df4549ecc6d7ad4a8
steve <steve@PC>
2024-03-13 18:08:00 +0700

    fisrt commit from Hoang

---
+++ src/main/test/com/supermarket/DAO/KhachHangDAOTest.java
@@ -0,0 +1,14 @@
+package com.supermarket.DAO;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KhachHangDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
