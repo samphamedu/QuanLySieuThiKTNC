1cbc532d8648fe447f1a0995dfb371c6b2cd19d5
Hoang0207 <giuse02072003@gmail.com>
2024-03-15 18:07:08 +0700

    KimHoang-PS24867: DonHangDAOTest

--- src/test/java/com/supermarket/DAO/DonHangDAOTest.java
+++ src/test/java/com/supermarket/DAO/DonHangDAOTest.java
@@ -2,13 +2,33 @@ package com.supermarket.DAO;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.supermarket.ENTITY.DonHang;
 
 public class DonHangDAOTest {
 
+	private DonHangDAO dao;
+	
+	@Before
+	public void init() {
+		dao = new DonHangDAO();
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		DonHangDAO dhdao = new DonHangDAO();
+        String maDH = "DH002";
+        dhdao.delete(maDH);
+        DonHang dh = dhdao.selectById(maDH);
+        assertNull(dh);   
+	}
+	
+	@After
+	public void tearDown() {
+		dao = null;
 	}
 
 }
