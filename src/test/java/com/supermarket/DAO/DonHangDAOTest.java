11bc30de2a3e4316e075155a4017a620347670d5
steve <steve@DESKTOP-EEDG5ET>
2024-03-16 12:57:28 +0700

    Kim Hoang: commit DonHangDAOTest (1)

--- src/test/java/com/supermarket/DAO/DonHangDAOTest.java
+++ src/test/java/com/supermarket/DAO/DonHangDAOTest.java
@@ -3,32 +3,57 @@ package com.supermarket.DAO;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.supermarket.ENTITY.DonHang;
 
 public class DonHangDAOTest {
-	//test
+
 	private DonHangDAO dao;
+	
+	@BeforeClass
+	public static void m1() {
+		System.out.println("Bắt đầu quá trình test DonHangDAOTest");
+	}
 	
 	@Before
 	public void init() {
 		dao = new DonHangDAO();
+		System.out.println("Chuẩn bị test");
 	}
 	
 	@Test
-	public void test() {
+	public void deleteTest1() {
 		DonHangDAO dhdao = new DonHangDAO();
         String maDH = "DH002";
         dhdao.delete(maDH);
         DonHang dh = dhdao.selectById(maDH);
+        assertNull(dh);  
+        System.out.println("TestCase deleteTeset1 thực hiện thành công");
+	}
+	
+	@Test
+	public void deleteTest2() {
+		DonHangDAO dhdao = new DonHangDAO();
+        String maDH = "DH003";
+        dhdao.delete(maDH);
+        DonHang dh = dhdao.selectById(maDH);
         assertNull(dh);   
+        System.out.println("TestCase deleteTeset2 thực hiện thành công");
 	}
 	
 	@After
 	public void tearDown() {
 		dao = null;
+		System.out.println("Kết thúc test");
+	}
+	
+	@AfterClass
+	public static void m2() {
+		System.out.println("Kết thúc quá trình test DonHangDAOTest \n\n\n");
 	}
 
 }
